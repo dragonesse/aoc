9e37072edd9c02e9cbb32952c84381ef81948c43
dragonesse <smoku@fakemail.com>
2017-01-29 21:10:00 +0100

    Day 12 part 2 resolved

--- day12.py
+++ day12.py
@@ -11,25 +11,24 @@ if(len(sys.argv) == 1):
 else:
     puzzle_file = sys.argv[1];
 
-registers = { 'a':0, 'b':0, 'c':0, 'd':0};
+registers = { 'a':0, 'b':0, 'c':1, 'd':0};
 commands  = [];
 
 #open file
 with open(puzzle_file, 'r') as puzzle_in:
     for cur_line in puzzle_in:
-        # print(cur_line.strip("\n"));
         commands.append(cur_line.strip("\n"));
-    # print (commands);
 puzzle_in.close();
 
 # process commands
 cmd_index = 0;
 while cmd_index < len(commands):
-    # print (commands[cmd_index]);
     cur_cmd = commands [cmd_index];
     # parse the command
     invoke = cur_cmd.split();
+    print (registers);
     print (invoke);
+
     if (invoke[0] == "cpy"):
         val_1 = 0;
         # b = a
@@ -42,15 +41,18 @@ while cmd_index < len(commands):
         registers[invoke[2]] = val_1;
         # increment the index
         cmd_index += 1;
+        continue;
 
     if (invoke[0] == "dec"):
         registers[invoke[1]] -= 1;
 
         cmd_index += 1;
+        continue;
 
     if (invoke[0] == "inc"):
         registers[invoke[1]] += 1;
         cmd_index += 1;
+        continue;
 
     if (invoke [0] == "jnz"):
         val_1, val_2 = 0, 0;
@@ -69,5 +71,6 @@ while cmd_index < len(commands):
             cmd_index += val_2;
         else:
             cmd_index += 1;
+        continue;
 
-print (registers);
+    print (registers);
