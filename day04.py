cb388638691ec84466871da77cc8b7a2d5a54aa3
dragonesse <smoku@fakemail.com>
2017-12-04 22:29:35 +0100

    part 2 resolved

--- day04.py
+++ day04.py
@@ -1,7 +1,7 @@
 import re;
 import sys;
 
-print("Day 1 puzzle: Inverse captcha");
+print("Day 4 puzzle: High-Entropy Passphrases");
 
 #read input
 puzzle_file = "";
@@ -11,35 +11,25 @@ if(len(sys.argv) == 1):
 else:
     puzzle_file = sys.argv[1];
 
-def index_to_compare (cur_index, offset):
-    calc_index = 0;
-    if (cur_index < offset):
-        calc_index = int(cur_index + offset);
-    else:
-        calc_index = offset - (2 * offset - cur_index);
-    return calc_index;
-
 #open file
+passphr_cntr = 0;
 with open(puzzle_file, 'r') as puzzle_in:
     for cur_line in puzzle_in:
-        digits = cur_line.strip("\n");
+        passphrase = [];
+        passphrase = cur_line.strip("\n").split();
+
+        found_equal = 0;
+
+        for i in range(len(passphrase)):
+            for j in range (i+1, len(passphrase) ):
+                if (sorted(list(passphrase[i])) == sorted(list(passphrase[j]))):
+                    # we found anagrams, no point to search futher...
+                    found_equal = 1;
+                    break;
+
+        if (found_equal == 0):
+            passphr_cntr += 1;
+
 puzzle_in.close();
 
-captcha_sum = 0;
-# calculate the offset to digit taken for comparison
-if (len(digits)%2 == 0):
-    index_offset = int(len(digits)/2);
-else:
-    print ("The input has odd number of digits, exiting");
-    sys.exit();
-
-# process captcha calculation
-digit_index = 0;
-for digit_index in range (len(digits)):
-    comp_index = index_to_compare (digit_index, index_offset);
-
-    if (digits[digit_index] == digits[comp_index]):
-        # incremetnt captcha sum
-        captcha_sum += int(digits [digit_index]);
-
-print ("The captcha sum is: %d" %(captcha_sum));
+print ("Number of valid passphrases: %d" %(passphr_cntr));
