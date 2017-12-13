e33373c4b4348113f4a943c6593fb6f81950580d
dragonesse <smoku@fakemail.com>
2017-12-13 02:34:51 +0100

    day 7, part 2 resolved

--- day07.py
+++ day07.py
@@ -32,13 +32,45 @@ with open(puzzle_file, 'r') as puzzle_in:
 
 puzzle_in.close();
 
-for key in weights.keys():
+def traverse_disc (single_disc, map_of_discs, weights_vals):
+    tot_balance = int(weights_vals[single_disc]);
+    if single_disc not in map_of_discs.keys():
+        # disc that does not carry anything
+        pass;
+    else:
+        for child_disc in map_of_discs[single_disc]:
+            tot_balance += traverse_disc (child_disc, map_of_discs, weights_vals);
+    return tot_balance;
+
+bal_weights = {};
+root = "";
+
+# find the root of the tree
+for k in weights.keys():
     found_parent = False;
     for child in maps.values():
-        if key in child:
+        if k in child:
             found_parent = True;
             break;
     if not found_parent:
-        print ("program %s is the root" %(key));
+        print ("program %s is the root" %(k));
+        root = k;
         break;
 
+key = root;
+
+while key:
+    unbalanced = [];
+    bal_weights = {};
+    for disc in maps[key]:
+        print ("traversing disc %s" %(disc));
+        bal_weights [disc] = traverse_disc(disc,maps,weights);
+
+        print ("balance weight of disc %s is %d" %(disc, bal_weights[disc]));
+    # get the unique value in the map of balances to find odd disc
+    countMap = {};
+    for w in bal_weights.values():
+        countMap[w] = countMap.get(w,0) + 1;
+    unbalanced = [ k for k, v in bal_weights.items() if countMap[v] == 1];
+    print ("unbalanced disc is: %s" %(unbalanced));
+    key = ''.join(unbalanced);
