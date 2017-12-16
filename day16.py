745a1d510c77c6f0f65c512e487fcc37f2fc10e9
dragonesse <smoku@fakemail.com>
2017-12-16 18:56:03 +0100

    attempt to day 2, part 2, under the test

--- day16.py
+++ day16.py
@@ -1,5 +1,6 @@
-import math;
+# import math;
 import sys;
+# import time;
 
 print("Day 16 puzzle: Permutation Promenade");
 
@@ -14,27 +15,29 @@ else:
 layout = [];
 
 #open file
+print ("reading input")
 with open(puzzle_file, 'r') as puzzle_in:
     layout = puzzle_in.read().strip("\n").split(",");
 
 puzzle_in.close();
+print ("closed input")
 
 def spin (num_places, order):
-    print ("spin: ", num_places)
+    # print ("spin: ", num_places)
     order = order[-num_places:]+order[:-num_places];
-    print (order);
+    # print (order);
     return order;
 
 def exchange (pos_a, pos_b, order):
-    print ("exchange: ", pos_a, pos_b);
+    # print ("exchange: ", pos_a, pos_b);
     tmp = order[pos_a];
     order [pos_a] = order [pos_b];
     order [pos_b] = tmp
-    print (order);
+    # print (order);
     return order;
 
 def partner (prog_a, prog_b, order):
-    print ("partner", prog_a, prog_b);
+    # print ("partner", prog_a, prog_b);
     a_pos = order.index(prog_a);
     b_pos = order.index(prog_b);
     return  exchange (a_pos, b_pos, order);
@@ -67,20 +70,43 @@ dancers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"];
 
 # analyse stream and track current child position
 
+num_iters = 1000000000;
+# num_iters = 100;
+first_dance = "";
+i = 0;
+while i < num_iters:
+    # start_time = time.time();
+    if not i%1000000:
+        print ("%d mln elapsed, %d to go" %(i, num_iters - i))
+    # print ("entering iteration ", i)
+    for step in layout:
+        # print(step);
+        sname = decode_move (step);
 
-for step in layout:
-    print(step);
-    sname = decode_move (step);
+        # dance
+        # print (dancers);
+        if sname == "s":
+            dancers = moves[sname](decode_args(sname,step),dancers);
+        else:
+            sarg = decode_args(sname,step);
+            dancers = moves[sname](sarg[0],sarg[1],dancers);
 
-    # dance
-    print (dancers);
-    if sname == "s":
-        dancers = moves[sname](decode_args(sname,step),dancers);
-    else:
-        sarg = decode_args(sname,step);
-        dancers = moves[sname](sarg[0],sarg[1],dancers);
+    if i == 0:
+        first_dance = dancers;
+        print (first_dance)
+    elif dancers == first_dance:
+        print ("after %d iterations the pattern repeats" %(i+1));
+        print (dancers);
+        # we need to calculate only remainder
+        i = num_iters - (num_iters % (i+1)) - 2
+        print ("increasing counter to %d" %(i));
+    i += 1;
+    # delta_t = time.time() - start_time;
 
-    # input ("press enter");
+    # print ("one iteration time: ", delta_t);
+    # print ("expected time to work: ", (delta_t * (1000000000 - i - 1)/60/60 ))
+
+
 
 print ("final position is ", ''.join(dancers));
 
