74d1305228df7add6618da6a50f3e28923faa02b
dragonesse <smoku@fakemail.com>
2017-12-19 20:58:00 +0100

    day 19, part 2 resolved + cleanup

--- day19.py
+++ day19.py
@@ -35,7 +35,6 @@ def find_direction (x,y,come_from, layout):
             new_dir = "left"
         elif re.match (r'[A-Z,-]', layout[y][x+1]):
             new_dir = "right";
-        print ("next direction is %s" %(new_dir));
         return new_dir;
 
     if come_from == "left" or come_from == "right":
@@ -44,25 +43,7 @@ def find_direction (x,y,come_from, layout):
             new_dir = "up"
         elif re.match (r'[A-Z,|]', layout[y + 1][x]):
             new_dir = "down";
-        print ("next direction is %s" %(new_dir));
         return new_dir;
-
-def is_end(x,y, come_from, layout):
-        is_end = False;
-        # the possible directions to search are left and right
-        if come_from  == "up":
-            if layout[y][x+1] == " " and layout[y][x-1] == " " and layout[y+1][x] == " ":
-                is_end = True;
-        elif come_from == "down" :
-            if layout[y][x+1] == " " and layout[y][x-1] == " " and layout[y-1][x] == " ":
-                is_end = True;
-        elif come_from == "left" :
-            if layout[y+1][x] == " " and layout[y - 1][x] == " " and layout[y][x+1] == " ":
-                is_end = True;
-        elif come_from == "right" :
-            if layout[y+1][x] == " " and layout[y - 1][x] == " " and layout[y][x-1] == " ":
-                is_end = True;
-        return is_end;
 
 # try to find the entrance
 while x in range(len(layout[y])):
@@ -70,23 +51,20 @@ while x in range(len(layout[y])):
         break;
     else:
         x += 1;
-print ("found entry at %d, %d" %(x,y));
 
 if layout[y][x] == '|':
     move_dir = "down";
 if layout[y][x] == '-':
     move_dir = "left";
 
-print ("new direction is %s" %(move_dir));
-
 letters = "";
-
+steps = 0;
 while True:
     # follow the rabit:
     while not is_terminator(layout[y][x]):
         # verify, we met letter:
+        steps += 1;
         if layout[y][x].isalpha():
-            print ("found a letter at %d %d" %(x,y));
             letters += layout[y][x];
         if move_dir == "down" :
             y += 1;
@@ -103,20 +81,22 @@ while True:
             print ("Exceeded array dimension");
             break;
 
-    print ("marched until %d %d" %(x, y));
-    # input ("press enter");
-
     move_dir = find_direction(x, y, move_dir, layout)
     if move_dir == "down":
         y += 1;
+        steps += 1
     elif move_dir == "up":
         y -= 1;
+        steps += 1
     elif move_dir == "left":
         x -= 1;
+        steps += 1
     elif move_dir == "right":
         x += 1;
+        steps +=1;
     else:
         print ("ended at %d %d" %(x, y));
         break;
 
 print ("traceroute is: %s" %(letters));
+print ("number of steps is: %d" %(steps));
