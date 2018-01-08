09fe77be78eb07530333b780ad4a5cd0c20d2df8
dragonesse <smoku@fakemail.com>
2018-01-08 20:54:58 +0100

    day13, part 2 resolved

--- day13.py
+++ day13.py
@@ -11,64 +11,49 @@ else:
     puzzle_file = sys.argv[1];
 
 scanners = {};
-positions = {};
 routes = {};
 #open file
 with open(puzzle_file, 'r') as puzzle_in:
     for cur_line in puzzle_in:
         [depth, rnge] = cur_line.strip("\n").split(':' );
         scanners[int(depth)] = rnge;
-        positions[int(depth)] = [int(depth),0];
         routes [int(depth)] = [x for x in range(int(rnge)-1)]+[x for x in range(int(rnge) - 1,0, -1)];
 puzzle_in.close();
-
-print (routes);
 
 def scanner_pos (route, delay):
     # a scanner moves in the loop
     # for range 4 and time of 6 cycles
     # it is present at positions:
     # [layer-n,0]-[layer-n,1]-[layer-n, 2]- [layer-n,3] [layer-n, 2] [layer-n, 1]
-
-    return  route[(delay%len(route))] ;
-
-severity = 0;
-
-ticks   = 0;
-pkt_layer = 0
+    return route[ delay%len(route)]
 
 max_layer = max(scanners.keys());
+lucky_layers = 0;
 
-while ticks <= max_layer:
+delay = 9;
+while lucky_layers != len(scanners):
+    # scanners move, pkt not (yet)
+    delay += 1
+
+    pkt_layer = 0
+    lucky_layers = 0;
+
     # clock tick k
     # you move (enter the firewall layer-n)
+    while pkt_layer <= max_layer:
 
-    pkt_layer = ticks;
-    print ("\ntick %d pkt_pos " %(ticks), [pkt_layer,0]);
+        # verify if you hit the scanner (or not)
+        if pkt_layer in scanners.keys():
+            sc_pos = scanner_pos (routes[pkt_layer],(delay+pkt_layer));
 
-    # you hit the scanner (or not)
-    if pkt_layer in scanners.keys():
-        sc_pos = positions[pkt_layer];
-        sc_pos = [positions[pkt_layer][0], scanner_pos (routes[pkt_layer],ticks)];
-        print ("tick %d sc_pos " %(ticks), sc_pos);
+            if not sc_pos:
+                print ("layer %d scanner detected me, what a shame!" %(pkt_layer));
+                break;
 
+            else:
+                lucky_layers += 1
 
-        if not sc_pos[1]:
-            print ("layer %d, scanner detected me, what a shame!" %(pkt_layer));
-            severity += pkt_layer * int(scanners[pkt_layer]);
-        else:
-            print ("layer %d, uff, no scanner detected me!" %(pkt_layer));
-        positions[pkt_layer] = sc_pos;
-    else:
-        print ("layer %d is free of scanners!" %(pkt_layer));
+        # clock ticks (k+1), pkt move to next layer
+        pkt_layer += 1;
 
-    # clock ticks (k+1)
-    ticks += 1;
-#you move, scanner move, clock ticks
-
-
-
-print ("traverse severity is %d" %(severity));
-
-
-
+print ("packet passed undetected with delay of %d" %(delay));
