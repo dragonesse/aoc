64d16aff789ea2ae3857574de246a00cc3450016
dragonesse <smoku@fakemail.com>
2018-12-01 23:37:35 +0100

    part 2: found first duplicated freq

--- day01.py
+++ day01.py
@@ -19,9 +19,22 @@ with open(puzzle_file, 'r') as puzzle_in:
     drift_steps = [int(cur_line) for cur_line in puzzle_in]
 puzzle_in.close()
 
-print (drift_steps)
-
 # calculate the total drift
 drift_sum = sum (drift_steps)
+print ("The total drift is: %d" %(drift_sum))
 
-print ("The total drift is: %d" %(drift_sum));
+# examine for the appearance of the duplicated result
+drift_res = [0]
+cur_drift = 0
+first_dup = False
+
+while not first_dup:
+    for delta in drift_steps:
+        cur_drift += delta
+        if cur_drift not in drift_res:
+            drift_res.append(cur_drift)
+        else:
+            print ("The first duplicated frequency is: %d" %(cur_drift))
+            first_dup = True
+            break
+
