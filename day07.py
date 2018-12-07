ee6e9003f55488c60e4ae8c184cb894a9a5e9528
dragonesse <smoku@fakemail.com>
2018-12-08 00:41:41 +0100

    evaluation if step is executable

--- day07.py
+++ day07.py
@@ -29,12 +29,22 @@ puzzle_in.close()
 for i in ikea_booklet.keys():
     print ("parent %s children %s" %(i,ikea_booklet[i]))
 
+def can_execute_step (step, instructions):
+    # we can proceed with a step, if its children are on list of values only once
+    print ("-----validating step %s " %(step))
+    num_hits = 0
+    for v in instructions.values():
+       print ("checking value %s for presence of %s" %(v,step))
+       if v.count(step) > 0 :
+            num_hits += 1
+
+    return num_hits == 0
+
 # find the master parent
 parent = []
 for p in ikea_booklet.keys():
-    if p not in set(''.join(ikea_booklet.values())):
+    if can_execute_step(p,ikea_booklet):
         parent.append(p)
-
 
 print ("following steps are independent: %s" %(parent))
 
