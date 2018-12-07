5c3af6163770f4b82d113dde8f11864be8276070
dragonesse <smoku@fakemail.com>
2018-12-07 01:05:08 +0100

    part 2: biggest safe region found

--- day06.py
+++ day06.py
@@ -1,5 +1,4 @@
 import sys
-import string
 
 print("Day 6 puzzle: Chronal Coordinates");
 
@@ -22,18 +21,6 @@ with open(puzzle_file, 'r') as puzzle_in:
         coordinates.append([x,y])
 puzzle_in.close()
 
-print (coordinates)
-
-sorted_by_y = sorted(coordinates, key=lambda x:x[1])
-sorted_by_x = sorted(coordinates, key=lambda x:x[0])
-[min_y, max_y] = [sorted_by_y[0][1],sorted_by_y[-1][1]]
-[min_x, max_x] = [sorted_by_x[0][0],sorted_by_x[-1][0]]
-
-[top_c,left_c,right_c,down_c] = [sorted_by_y[0],sorted_by_x[0],sorted_by_x[-1],sorted_by_y[-1]]
-
-print ("top left right down", top_c,left_c,right_c,down_c)
-print ("will process from: ", [min_x, min_y], "to ", [max_x, max_y])
-
 def get_cab_distance (start_x,start_y,end_x,end_y):
     return abs(end_x - start_x) + abs(end_y - start_y)
 
@@ -45,6 +32,14 @@ def get_x_from_key (key):
 
 def get_y_from_key (key):
     return int(key[-1])
+
+# part 1
+sorted_by_y = sorted(coordinates, key=lambda x:x[1])
+sorted_by_x = sorted(coordinates, key=lambda x:x[0])
+[min_y, max_y] = [sorted_by_y[0][1],sorted_by_y[-1][1]]
+[min_x, max_x] = [sorted_by_x[0][0],sorted_by_x[-1][0]]
+
+[top_c,left_c,right_c,down_c] = [sorted_by_y[0],sorted_by_x[0],sorted_by_x[-1],sorted_by_y[-1]]
 
 # from minx,miny to maxx,maxy
 # get all the points and try to calculate distance to
@@ -59,19 +54,17 @@ for y in range(min_y,max_y + 1):
             cur_dist = get_cab_distance(x,y,target_loc[0],target_loc[1])
             # we hit the coordinate, no point to process
             if cur_dist == 0:
-                print ([x,y], " hit the exact coordinate", target_loc)
                 winning_loc = target_loc
                 dist_to_comp = 0
                 break
+            # new closest
             elif cur_dist < dist_to_comp:
                 dist_to_comp = cur_dist
                 winning_loc = target_loc.copy()
+            # a tie, reset the winner
             elif cur_dist == dist_to_comp:
-                print ([x,y], " has a tie, reset winner")
                 winning_loc = []
-                # break
         if len(winning_loc):
-            print ([x,y], " is closest to ", winning_loc, " the distance is ", dist_to_comp )
             coord_key = convert_coord_to_string(winning_loc[0],winning_loc[1])
             if coord_key not in areas:
                 areas[coord_key] = 1
@@ -79,20 +72,57 @@ for y in range(min_y,max_y + 1):
                 areas[coord_key] = areas[coord_key] + 1
 
 # border areas are always infinite, remove them from list of entries
-print (areas)
-
 for c in areas.keys():
     if get_x_from_key(c) == min_x or get_x_from_key(c) == max_x:
         areas[c] = -1
     elif get_y_from_key(c) ==min_y or get_y_from_key(c) == max_y:
         areas[c] = -1
+print (areas)
 
 print ("The biggest area is ", max(areas.values()))
 
+# part two
+def is_safe (x, y, safe_dist, coordinates):
+    dist_to_comp = 0
+    for target_loc in coordinates:
+        dist_to_comp += get_cab_distance(x,y,target_loc[0],target_loc[1])
 
+    return dist_to_comp < safe_dist
 
+# calculate safe region
+regions = []
+# safe_distance = 32
+safe_distance = 10000
 
+for [cx,cy] in coordinates:
+    # build safe areas around coordinates
+    print ("\nchecking around coordinate ", [cx,cy])
+    safe_flag = True
+    side_len = 1
+    region_size = 0
 
+    while safe_flag:
+        safe_flag = False
+        if side_len == 1:
+            if not is_safe(cx,cy,safe_distance,coordinates):
+                print ("can't build around coordinates, if they are unsafe")
+                break
+            else:
+                region_size += 1
 
+        for y in range(cy-side_len, cy+side_len+1):
+            if y == cy-side_len or y == cy+side_len:
+                for x in range(cx-side_len, cx+side_len+1):
+                    if is_safe(x, y, safe_distance, coordinates):
+                        safe_flag = True
+                        region_size += 1
+            else:
+                for x in [cx-side_len, cx+side_len]:
+                    if is_safe(x, y, safe_distance, coordinates):
+                        safe_flag = True
+                        region_size += 1
+        side_len += 1
+    print ("overall region size is: ", region_size)
+    regions.append(region_size)
 
-
+print ("The biggest safe region is: ", max(regions))
