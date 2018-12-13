2840edc2dfc4f8f988a4759e411196e5fe018207
dragonesse <smoku@fakemail.com>
2018-12-14 00:39:59 +0100

    part 2: the last car [6,18] is not good

--- day13.py
+++ day13.py
@@ -1,5 +1,5 @@
 import sys
-import re
+import copy
 
 print("Day 13 puzzle: Mine Cart Madness");
 
@@ -24,7 +24,7 @@ puzzle_in.close()
 [print (''.join(tracks[i])) for i in range(len(tracks))]
 
 class Cart:
-    def __init__(self, x, y, direction):
+    def __init__(self, x, y, direction=""):
         self.x = x
         self.y = y
         self.direction = direction
@@ -116,18 +116,6 @@ class Cart:
     def get_cur_pos (self):
         return [self.x, self.y]
 
-    def get_next_pos (self):
-        xnext,ynext = -1,-1
-        if self.direction == ">":
-            xnext = self.x + 1
-        if self.direction == "<":
-            xnext = self.x - 1
-        if self.direction == "^":
-            ynext = self.y - 1
-        if self.direction == "v":
-            ynext = self.y + 1
-        return [xnext,ynext]
-
     def make_next_move (self, track_ahead):
         # if track_ahead == "-" or track_ahead == "|":
         #     pass
@@ -165,13 +153,32 @@ for i in range(len(tracks)):
             active_cars[-1].print_coordinates()
             tracks[i][j] = fill_track(track_piece)
 
+# cur_cars = copy.deepcopy(active_cars)
+# active_cars[3].go_straight()
+# active_cars[4].go_straight()
+
+# for i in range(len(cur_cars)):
+#     print ("\n")
+#     cur_cars[i].print_coordinates()
+#     active_cars[i].print_coordinates()
+
+# active_cars.remove(Cart(1,0,">"))
+
+# print ("delete")
+# for i in range(len(active_cars)):
+#     # print ("\n")
+#     # cur_cars[i].print_coordinates()
+#     active_cars[i].print_coordinates()
+
+
+# sys.exit()
 # move on
 collison = False
 tick = 0
-while not collison:
+while len(active_cars) > 1:
     print ("------starting tick %d " %(tick))
     future_pos = []
-
+    # cur_cars = copy.deepcopy(active_cars)
     for i in sorted(active_cars):
         print ("\n")
         i.print_coordinates()
@@ -179,10 +186,19 @@ while not collison:
         road_ahead = tracks[cur_pos[1]][cur_pos[0]]
         i.make_next_move(road_ahead)
         i.print_coordinates()
-        if i.get_cur_pos() in future_pos:
+        cur_pos = i.get_cur_pos()
+        if cur_pos in future_pos:
             print ("found collison, the coordinates are ", i.get_cur_pos())
-            collison = True
-            break
+            # active_cars.remove(Cart(1,0,">"))
+            active_cars.remove(Cart(cur_pos[0],cur_pos[1],""))
+            active_cars.remove(Cart(cur_pos[0],cur_pos[1],""))
+            future_pos.remove([cur_pos[0],cur_pos[1]])
+            # collison = True
+            # break
+
         else:
             future_pos.append(i.get_cur_pos())
     tick += 1
+
+print ("the last car left: ")
+active_cars[0].print_coordinates()
