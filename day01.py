edb1656fec98c9e80f79a30efb72de0997431532
dragonesse <smoku@fakemail.com>
2019-12-01 19:59:46 +0100

    day 1, part 2 resolved

--- day01.py
+++ day01.py
@@ -21,8 +21,24 @@ puzzle_in.close()
 
 # calculate the total fuel
 def calculate_fuel (mass):
-    return math.floor((mass/3))-2
+    fuel= math.floor((mass/3))-2
+    return fuel if fuel > 0 else 0
+tot_fuel_per_unit =[]
+
+for unit_num in range(len(loaded_mass)):
+    mass = loaded_mass[unit_num]
+
+    fuel = calculate_fuel (mass)
+    tot_fuel_per_unit += [fuel]
+    while fuel > 0:
+        mass = fuel
+        fuel = calculate_fuel (mass)
+        tot_fuel_per_unit[unit_num] += fuel
+
+    tot_fuel_per_unit[unit_num] += fuel
 
 required_fuel = sum(map(calculate_fuel,loaded_mass))
+print ("The rocket requires %d fuel for part 1" %(required_fuel))
 
-print ("The rocket requires %d fuel" %(required_fuel))
+required_fuel = sum(tot_fuel_per_unit)
+print ("The rocket requires %d fuel for part 2" %(required_fuel))
