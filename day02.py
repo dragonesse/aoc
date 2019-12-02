69a5a98f00f6ea6548995c592965c8a7f497c78d
dragonesse <smoku@fakemail.com>
2019-12-02 23:56:07 +0100

    day 2, part 2 works

--- day02.py
+++ day02.py
@@ -10,7 +10,6 @@ if(len(sys.argv) == 1):
 else:
     puzzle_file = sys.argv[1];
 
-
 #open file
 instr_list = []
 
@@ -18,16 +17,14 @@ with open(puzzle_file, 'r') as puzzle_in:
     instr_list = [int(instr) for instr in puzzle_in.readline().strip("\n").split(",")]
 puzzle_in.close()
 
-print (instr_list)
+original_list = instr_list.copy()
 
 # perform addition
 def add (val1, val2, position):
-    print ("add of %d, %d. Result stored on pos %d" %(val1,val2,position))
     insert_result (val1+val2,position)
     return
 
 def multiply (val1, val2, position):
-    print ("mul of %d, %d. Result stored on pos %d" %(val1,val2,position))
     insert_result (val1*val2, position)
     return
 
@@ -38,23 +35,33 @@ def insert_result (val, position):
         print ("Invalid position %d to insert at" %(position))
     return
 
-def modify_input_data():
-    instr_list[1] = 12
-    instr_list[2] = 2
+def modify_input_data(noun=12,verb=2):
+    instr_list[1] = noun
+    instr_list[2] = verb
+    return
 
 def get_val_at_pos (position):
     return instr_list[position] if position <= len (instr_list) else -1
+
+def reset_memory():
+    for i in range(len(original_list)):
+        instr_list[i] = original_list[i]
+    return
 
 instructions = {
     "1" : add,
     "2" : multiply
     }
-
-pos = 0
 offset = 4
-modify_input_data()
-while instr_list[pos] != 99:
 
-    instructions[str(instr_list[pos])](get_val_at_pos(instr_list[pos+1]),get_val_at_pos(instr_list[pos+2]),instr_list[pos+3])
-    print (instr_list)
-    pos += offset
+for noun in range(len(instr_list)):
+    for verb in range (len(instr_list)):
+        pos = 0
+        reset_memory()
+        modify_input_data(noun,verb)
+        while instr_list[pos] != 99:
+            instructions[str(instr_list[pos])](get_val_at_pos(instr_list[pos+1]),get_val_at_pos(instr_list[pos+2]),instr_list[pos+3])
+            pos += offset
+        if instr_list[0] == 19690720:
+            print ("noun %d, verb %d, answer %d" %(noun,verb, 100*noun+verb))
+            sys.exit()
