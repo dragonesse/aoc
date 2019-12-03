994d1ab1bda0b4cf5c2f4312d50bcb418a8088c1
dragonesse <smoku@fakemail.com>
2019-12-03 22:47:48 +0100

    day 3, part 2 resolved

--- day03.py
+++ day03.py
@@ -19,10 +19,6 @@ with open(puzzle_file, 'r') as puzzle_in:
     wire2 = puzzle_in.readline().strip("\n").split(",")
 puzzle_in.close()
 
-print (wire1)
-print (wire2)
-# print (wire1[1:])
-
 cordinates_w1 = []
 cordinates_w2 = []
 def calc_manhattan_dist (x1, y1, x2, y2):
@@ -32,7 +28,6 @@ def transpond (guideline,location_arry):
     direction = guideline[0]
     length = int(guideline[1:])
     start_coordinate = get_last_pos(location_arry)
-    # print ("last coordinate", start_coordinate)
     if direction == "U":
         end_coordinate = (start_coordinate[0],start_coordinate[1] + length )
     elif direction == "D":
@@ -47,10 +42,8 @@ def get_last_pos (location_arry):
     return location_arry[-1][1] if len(location_arry)>0 else (0,0)
 
 def is_vertical(section):
-    print("is_vertical", section[0][0] == section[1][0], section)
     return section[0][0] == section[1][0]
 def is_horizontal(section):
-    print("is_horizontal", section[0][1] == section[1][1], section)
     return section[0][1] == section[1][1]
 def is_inside(p1, ps, pe):
     return ((p1 >= ps) and (p1 <= pe)) or ((p1 <= ps) and (p1 >= pe))
@@ -77,23 +70,32 @@ for guideline in wire1:
 for guideline in wire2:
     cordinates_w2.append(transpond(guideline,cordinates_w2))
 
-print (cordinates_w1)
-print (cordinates_w2)
-
-# print (is_crossing([(0, 0), (20, 0)], [(1,-5),(1,20)]))
-# print (is_crossing([(0, 0), (20, 0)], [(-1,-5),(-1,20)]))
-# print (is_crossing([(0, -5), (0, 20)], [(-1,-5),(10,-5)]))
-
-min_distance = []
+min_dist_by_len = []
+min_dist_by_time = []
 start_point = (0,0)
-for section2 in cordinates_w2:
-    for section1 in cordinates_w1:
-        print ("==== checking pair: ", section2, section1)
+dist_w1 = 0
+dist_w2 = 0
+for i in range(len(cordinates_w2)):
+    section2 = cordinates_w2[i]
+    dist_w2 += int(wire2[i][1:])
+    dist_w1 = 0
+    for k in range(len(cordinates_w1)):
+        section1 = cordinates_w1[k]
+        dist_w1 += int(wire1[k][1:])
         intersection = calc_cross_point(section2,section1)
         if intersection is not None:
-            print ("intersecting at: ", intersection)
+
             dist = calc_manhattan_dist(intersection[0],intersection[1], start_point[0],start_point[1])
             if dist>0:
-                min_distance.append(dist)
+                min_dist_by_len.append(dist)
+                # decrement distance
+                if is_horizontal (section1):
+                    dist_w1_tmp = dist_w1 - abs(section1[1][0] - intersection[0])
+                    dist_w2_tmp = dist_w2 - abs(section2[1][1] - intersection[1])
+                else:
+                    dist_w1_tmp = dist_w1 - abs(section1[1][1] - intersection[1])
+                    dist_w2_tmp = dist_w2 - abs(section2[1][0] - intersection[0])
+                min_dist_by_time.append(dist_w2_tmp + dist_w1_tmp)
 
-print("the min distance is", min(min_distance))
+print("the min distance by length is", min(min_dist_by_len))
+print ("the min distance by time is", min(min_dist_by_time))
