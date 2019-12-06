6efdf018ab87fe66cfe823dfec45e3bf9207fb44
dragonesse <smoku@fakemail.com>
2019-12-06 03:04:40 +0100

    day 5, part 2 resolved

--- day05.py
+++ day05.py
@@ -37,12 +37,23 @@ def multiply (val1, val2, position):
     insert_result (val1*val2, position)
     return
 
+def less_than (val1, val2, position):
+    insert_result (int(val1<val2),position)
+    return
+
+def equals (val1, val2, position):
+    insert_result (int(val1 == val2),position)
+    return
+
+def jump_if_true (val1,val2,pointer):
+    return val2 if val1 != 0 else pointer
+
+def jump_if_false (val1,val2,pointer):
+    return val2 if val1 == 0 else pointer
+
 def save_input (inp, position):
     insert_result(inp, position)
     return
-
-def throw_output (position):
-    return instr_list [position]
 
 def insert_result (val, position):
     if position <= len(instr_list):
@@ -60,35 +71,55 @@ def get_val_at_pos (position):
     return instr_list[position] if position <= len (instr_list) else -1
 
 def calc_offset (instr):
-    return 4 if is_two_arg_instr(instr) else 2
+    if is_three_arg_instr(instr):
+        return 4
+    elif is_two_arg_instr (instr):
+        return 3
+    else:
+        return 2
+
+def is_three_arg_instr (instr):
+    return instr in instructions.keys()
 
 def is_two_arg_instr (instr):
-    return instr in ["01", "02"]
+    return instr in orders.keys()
 
 instructions = {
     "01" : add,
-    "02" : multiply
+    "02" : multiply,
+    "07" : less_than,
+    "08" : equals
     }
 
+orders ={
+    "05" : jump_if_true,
+    "06" : jump_if_false
+}
+
 pos = 0
-test_inp = 1
+test_inp = 5
 test_out =[]
 while instr_list[pos] != "99":
-
+    set_offset = True
     [modes , instr] = parse_instr(instr_list[pos])
-    print ("position %d, instruction %s %s, arg1 %s" %(pos, ''.join(modes), instr, instr_list[pos+1]))
     v1 = get_value(int(modes[-1]),int(instr_list[pos+1]))
-    print ("the value 1: %d" %(v1))
-    if is_two_arg_instr(instr):
+    if is_three_arg_instr(instr):
         v2 = get_value(int(modes[-2]),int(instr_list[pos+2]))
-        print ("the value 2: %d the registry: %d" %(v2, int(instr_list[pos+3])))
         instructions[instr](v1,v2,int(instr_list[pos+3]))
+    elif is_two_arg_instr(instr):
+        v2 = get_value(int(modes[-2]),int(instr_list[pos+2]))
+        next_pos = orders[instr](v1,v2,pos)
+        if next_pos != pos:
+            pos = next_pos
+            set_offset = False
     elif instr == "03":
+        v1 = int(instr_list[pos+1])
         save_input(test_inp,v1)
     else:
         test_out += [v1]
 
-    offset = calc_offset(instr)
-    pos += offset
+    if set_offset:
+        offset = calc_offset(instr)
+        pos += offset
 
-print (test_out)
+print ("tests results: ",test_out)
