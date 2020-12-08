b694d3a02d4d8f1332129b39d0c54eae0f4d1f54
dragonesse <smoku@fakemail.com>
2020-12-08 20:47:50 +0100

    day 08, part 2 works

--- day08.py
+++ day08.py
@@ -11,15 +11,14 @@ else:
     puzzle_file = sys.argv[1];
 
 #open file
-
 program_code = []
-
 
 with open(puzzle_file, 'r') as puzzle_in:
     for cur_line in puzzle_in:
         program_code.append(cur_line.strip().split(' '))
 
 puzzle_in.close()
+
 
 visited_instr = [False for i in range(len(program_code))]
 
@@ -45,6 +44,16 @@ instructions ={
     "nop" : do_nop
 }
 
+def clear_memory(mem):
+    mem.global_acc = 0
+    mem.code_index = 0
+
+def clear_path ():
+    return [False for i in range(len(program_code))]
+
+def load_code(pc):
+    return pc.copy()
+
 mem = memory_state()
 
 while not visited_instr[mem.code_index]:
@@ -54,3 +63,37 @@ while not visited_instr[mem.code_index]:
     instructions[instr](val,mem)
 
 print ("part 1: accumulator value: %d" %mem.global_acc)
+
+subs_list = []
+
+for i in range(len(program_code)):
+    if program_code[i][0] in ["jmp","nop"]:
+        subs_list += [i]
+
+for inst_ind in subs_list:
+    clear_memory(mem)
+    visited_instr = clear_path()
+    upd_code = program_code
+    org_inst = ""
+    if program_code[inst_ind][0] == "jmp":
+        upd_code[inst_ind][0] = "nop"
+        org_inst = "jmp"
+    else:
+        upd_code[inst_ind][0] = "jmp"
+        org_inst ="nop"
+
+
+    while (not visited_instr[mem.code_index]):
+        instr = upd_code[mem.code_index][0]
+        val = int(upd_code[mem.code_index][1])
+
+        visited_instr[mem.code_index] = True
+        instructions[instr](val,mem)
+
+        if (mem.code_index ==len(upd_code)):
+            print ("seems you reached the last line")
+            print("part 2: code index: %d acc val: %d" %(mem.code_index,mem.global_acc))
+            break
+    # program_code and updated_code share the same reference, so
+    # a cleanup is needed here
+    program_code[inst_ind][0] = org_inst
