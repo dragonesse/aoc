855fdbdc6302b22f5b1f7844b8349dbc1c028aa6
dragonesse <smoku@fakemail.com>
2020-12-25 18:47:13 +0100

    Day 18, part 2 solved

--- day18.py
+++ day18.py
@@ -17,15 +17,13 @@ def evaluate_result (equation):
     res = 0
     # parse the equation
     while equation.count('(') > 0:
-        # print("======collapsing %s" %equation)
         parse_res = re.findall(r"(\([0-9]+[^(,)]+[0-9]+\))",equation)
         for e in parse_res:
            tmp = evaluate_result_simple_ops(e[1:-1])
-           # print ("bracket eval res: %s" %str(tmp))
            equation = equation.replace(e,str(tmp))
-           # print ("shortened equation: %s" %equation)
     res = evaluate_result_simple_ops(equation)
     return res
+
 
 def evaluate_result_simple_ops (equation):
     tokens = []
@@ -34,10 +32,9 @@ def evaluate_result_simple_ops (equation):
         return None
 
     tokens = equation.split(" ")
+    # process operations from left to right
     res = int(tokens[0])
-    # print ("evaluate_result_simple_ops for %s" %equation)
     for ti in range(1,len(tokens),2):
-        # print ("processing token %d result by now: %d" %(ti,res))
         if tokens[ti] == '+':
             res += int(tokens[ti+1])
         elif tokens[ti] == '*':
@@ -49,5 +46,47 @@ answer = 0
 for exercise in homework:
     answer += evaluate_result (exercise)
 
-print ("the answer to the universe is %d" %answer)
+print ("Part 1: the answer to the universe is %d" %answer)
 
+def evaluate_result_simple_ops_add_precedence (equation):
+    tokens = []
+    if ('(' in equation) or (')' in equation):
+        print ("not supposed to process nested levels")
+        return None
+
+    tokens = equation.split(" ")
+
+    # resolve all addition at first
+    while '+' in tokens:
+        for ti in range(1,len(tokens),2):
+            if tokens[ti] == '+':
+                res = int(tokens[ti+1])+int(tokens[ti-1])
+                tokens[ti-1] = str(res)
+                del tokens [ti+1]
+                del tokens [ti]
+                break
+
+    # process multiplication
+    res = int(tokens[0])
+    for ti in range(1,len(tokens),2):
+        if tokens[ti] == '*':
+            res *= int(tokens[ti+1])
+
+    return res
+
+def evaluate_result_new_rules (equation):
+    res = 0
+    # parse the equation
+    while equation.count('(') > 0:
+        parse_res = re.findall(r"(\([0-9]+[^(,)]+[0-9]+\))",equation)
+        for e in parse_res:
+           tmp = evaluate_result_simple_ops_add_precedence (e[1:-1])
+           equation = equation.replace(e,str(tmp))
+    res = evaluate_result_simple_ops_add_precedence (equation)
+    return res
+
+answer = 0
+for exercise in homework:
+    answer += evaluate_result_new_rules  (exercise)
+
+print ("Part 2: the answer to the universe is %d" %answer)
