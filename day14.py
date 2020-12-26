8ed721c2ffd4b3f17931eb658339b6a0414c0bdc
dragonesse <smoku@fakemail.com>
2020-12-26 21:37:36 +0100

    day 14, part 2 solved

--- day14.py
+++ day14.py
@@ -34,6 +34,22 @@ def extract_bits_to_replace(mask):
             break
     return bits
 
+def extract_bits_with_val (mask, val):
+    offset =0
+    bits = []
+    while offset < 36:
+        pos = mask.find(val,offset)
+        if pos  != -1:
+            bits.append (pos)
+            offset = pos + 1
+        else:
+            break
+    return bits
+
+def extract_floating_bits(mask):
+    return extract_bits_with_val (mask, 'X')
+
+
 def update_val_by_bitmask (value, bits2replace):
     bin_string_val = list(format(value, '036b'))
     for repl in bits2replace:
@@ -42,22 +58,73 @@ def update_val_by_bitmask (value, bits2replace):
 
 
 for command in program  :
-    print ("processing command: %s" %command )
     if command.startswith("mask"):
         mask = command.split(' ')[2]
         edit_list = extract_bits_to_replace (mask )
     else:
         mem_ind = int(command.split('[')[1].split(']')[0])
         mem_val_dec = int(command.split(' ')[2])
-        print (mask)
-        print (format(mem_val_dec, '036b'))
 
         memory [mem_ind] = update_val_by_bitmask(mem_val_dec,edit_list)
-        print (format(memory[mem_ind],'036b'))
 
 mem_sum =0
 for m in memory.keys():
     mem_sum += memory  [m]
 
-print  ("part 1: sum of memory cells is: %d" %mem_sum   )
+print  ("part 1: sum of memory cells is: %d" %mem_sum )
 
+def update_val_by_bitmask_rule2 (value, bits2replace):
+    bin_string_val = list(format(value, '036b'))
+    for repl in bits2replace:
+        bin_string_val[repl]= '1'
+    return int(''.join(bin_string_val),2)
+
+def extract_mask_from_base(value, bits2ignore):
+    bin_string_val = list(format(value, '036b'))
+    for ign_ind in bits2ignore:
+        bin_string_val[ign_ind]= '0'
+    return ''.join(bin_string_val)
+
+def calculate_base_wo_mask (value, bits2ignore ):
+    bin_string_val = extract_mask_from_base(value,bits2ignore)
+    base_val = int(bin_string_val,2)
+    return  base_val
+
+def generate_addr_at_float_ind (base_addr, float_mask_ind):
+    return [base_addr +2**(35-float_mask_ind) ,base_addr ]
+
+def generate_heap (base_addr_list, float_mask_list ):
+    res =[]
+    fi = float_mask_list.pop(0)
+
+    for ba in base_addr_list:
+        tmp = generate_addr_at_float_ind (ba, fi)
+        res+=tmp
+
+    if len(float_mask_list ) > 0:
+        return generate_heap (res, float_mask_list)
+    return res
+
+memory = {}
+for command in program  :
+    if command.startswith("mask"):
+        mask = command.split(' ')[2]
+        edit_list = extract_bits_with_val (mask,'1' )
+    else:
+        float_list = extract_floating_bits(mask)
+
+        mem_ind = int(command.split('[')[1].split(']')[0])
+        mem_val_dec = int(command.split(' ')[2])
+
+        mem_addr_upd = update_val_by_bitmask_rule2(mem_ind,edit_list)
+        mem_addr_base = calculate_base_wo_mask (mem_addr_upd,float_list)
+
+        heap = generate_heap ([mem_addr_base], float_list)
+        for h in heap :
+            memory [h] = mem_val_dec
+
+mem_sum =0
+for m in memory.keys():
+    mem_sum += memory  [m]
+
+print  ("part 2: sum of memory cells is: %d" %mem_sum, mem_sum> 1330204164446)
