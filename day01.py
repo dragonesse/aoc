a24045d32ed2f7db7939dbcba9620d2a0d4bed3c
dragonesse <smoku@fakemail.com>
2022-12-01 22:42:04 +0100

    Day 1, part 2

--- day01.py
+++ day01.py
@@ -14,14 +14,11 @@ else:
 #read file
 food = ir.read_multiline_records_as_list_entries(puzzle_file)
 
-#level 1 solution
-num_increments = 0
 supply_list = []
 for elf_inventory in food:
     supply_list.append(sum(int(cal) for cal in elf_inventory))
 
-print (supply_list)
-
 print ("Part 1: the most available calories is: {}".format(max(supply_list)))
 
-
+top3 = sorted(supply_list)[-3:]
+print ("Part 2: the sum of calories for top 3 equipped elves is: {}".format(sum(top3)))
