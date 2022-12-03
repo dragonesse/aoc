142f5af1b0d228659995846c58faad26b8dcb36f
dragonesse <smoku@fakemail.com>
2022-12-04 00:36:36 +0100

    Day 3, part 2

--- day03.py
+++ day03.py
@@ -16,24 +16,18 @@ backpacks = ir.read_oneline_records_as_list_entries(puzzle_file)
 
 def get_priority (item):
     priority = 0
-    if item == '':
-        print ("unexpected item")
-    elif item.islower():
+    if item.islower():
         priority = ord(item) - 96
     else:
         priority = ord(item) - 38
-    print ("item {}:{}".format(item,priority))
     return priority
 
 def find_duplicates (load):
     items_per_compartment = int(len(load)/2)
-    duplicates = ''
 
     for item in load[:items_per_compartment]:
-        if (item in load[items_per_compartment:]) and (item not in duplicates):
-                duplicates = item
-                break
-    return duplicates
+        if item in load[items_per_compartment:]:
+            return item
 
 
 priorities = 0
@@ -43,9 +37,14 @@ for carry in backpacks:
 
 print ("Part 1: the total priority is: {}".format(priorities))
 
-sys.exit(0)
-score = 0
-for game in secret_book:
-    score += determine_game_outcome(choose_your_move(game))
+priorities = 0
 
-print ("Part 2: your score is: {}".format(score))
+for team_num in range(0,len(backpacks),3):
+    team_check = 0
+    team_load = sorted(backpacks[team_num : team_num+3],key=len)
+    for item in team_load [0] :
+        if item in  team_load [1] and item in team_load [2]:
+            priorities += get_priority (item)
+            break
+
+print ("Part 2: the total priority is: {}".format(priorities))
