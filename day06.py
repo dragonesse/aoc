3d3e8a9fc0ddae3d04a06ebf9bf8f0d34660e9eb
dragonesse <smoku@fakemail.com>
2022-12-06 21:21:26 +0100

    Day 6, part 2

--- day06.py
+++ day06.py
@@ -14,34 +14,37 @@ else:
 #read file
 data_stream = ir.read_oneline_records_as_list_entries(puzzle_file)
 
-char_offset = 0
-uniq_chars = 0
-print (data_stream[0])
-
-def is_char_uniq_in_chunk (char, chunk):
-    if chunk.count(char)>1:
-        print ("\'{}\' repeats in {}".format(char,chunk))
-        return False
-    else:
-        print ("\'{}\' is unique in {}".format(char,chunk))
-        return True
-
 # convert to single string
 data_stream = ''.join(data_stream)
 
-for char_offset in range(len(data_stream)):
 
-    while uniq_chars < 4:
-        char = data_stream[char_offset+uniq_chars]
-        print ("Processing \'{}\' offset {}".format (char,char_offset+uniq_chars))
-        if is_char_uniq_in_chunk(char,data_stream[char_offset:char_offset+4]):
-            print ("Found uniq char, examine next entry")
-            uniq_chars += 1
-        else:
-            print ("Found duplicate, restarting")
-            uniq_chars = 0
+def is_char_uniq_in_chunk (char, chunk):
+    return not chunk.count(char)>1
+
+def find_first_uniq_set (stream, chunk_len):
+    char_offset = 0
+    uniq_chars = 0
+
+    for char_offset in range(len(stream)):
+
+        while uniq_chars < chunk_len:
+            char = data_stream[char_offset+uniq_chars]
+            if is_char_uniq_in_chunk(char,data_stream[char_offset:char_offset+chunk_len]):
+            # Found uniq char, examine next entry
+                uniq_chars += 1
+            else:
+                # Found duplicate, restarting
+                uniq_chars = 0
+                break
+        if uniq_chars == chunk_len:
+            # set complete
             break
-    if uniq_chars == 4:
-        break
+    return char_offset + chunk_len
 
-print ("Part 1: the first start-of-packet marker is after {} character".format(char_offset+4))
+start_of_packet = find_first_uniq_set (data_stream,4)
+
+print ("Part 1: the first start-of-packet marker is after {} character".format(start_of_packet))
+
+start_of_message = find_first_uniq_set (data_stream,14)
+
+print ("Part 2: the first start-of-message marker is after {} character".format(start_of_message))
